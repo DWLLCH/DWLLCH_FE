7c25e79477f44072b473168191c5f7a585f7be42
benumarsonbush <syfaceking@gmail.com>
2026-08-20 03:49:04 +0900

    fix: BriefingCard 매핑 키 검사를 hasOwnProperty 기반으로 변경

--- src/components/BriefingCard.jsx
+++ src/components/BriefingCard.jsx
@@ -4,8 +4,12 @@ import '../styles/BriefingCard.css';
 // color/icon 기본값은 BE Briefing 모델 기본값(blue/document)과 맞춤
 // BE가 매핑에 없는 값을 내려줄 수도 있어서, 지원하지 않는 값이면 기본값으로 대체함 (깨진 이미지 방지)
 function BriefingCard({ color, icon, title, onClick }) {
-  const resolvedColor = BRIEFING_BOX_IMAGES[color] ? color : 'blue';
-  const resolvedIcon = BRIEFING_ICONS[icon] ? icon : 'document';
+  const resolvedColor = Object.prototype.hasOwnProperty.call(BRIEFING_BOX_IMAGES, color)
+    ? color
+    : 'blue';
+  const resolvedIcon = Object.prototype.hasOwnProperty.call(BRIEFING_ICONS, icon)
+    ? icon
+    : 'document';
 
   return (
     <div className="briefing-card">
